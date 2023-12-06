9dc5cdbc71a75a26a6dee2452546beb0cb6e85de
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2023-12-06 13:48:50 +0100

    rest of basic subrepo commands

--- featurePatch/git.py
+++ featurePatch/git.py
@@ -8,6 +8,8 @@ from plumbum import local
 from .util import configuration, logger
 
 git = local['git']
+# TODO: export to configs
+SUBREPO_VERBOSITY = "-dv"
 
 
 def run_command(cmd):
@@ -17,14 +19,15 @@ def run_command(cmd):
     logger().info(output)
 
 
-def navigate_to_container_root():
-    os.chdir(configuration()["container_git_root"])
+def navigate_to(path):
+    logger().info(f"chdir {path}")
+    os.chdir(path)
 
 def push_subrepo():
     # Navigate to the root of the container and push the changes of the subrepository
-    navigate_to_container_root()
+    navigate_to(configuration()["container_git_root"])
     # Push the changes to the subrepository and log the generated output
-    cmd = git["subrepo", "-dv", "push", configuration()["feature_git_root"]]
+    cmd = git["subrepo", SUBREPO_VERBOSITY, "push", configuration()["feature_git_root"]]
     run_command(cmd)
     # TODO: Error handling?
 
@@ -32,9 +35,9 @@ def push_subrepo():
 def pull_subrepo():
     # Navigate to the root of the container and pull the changes of the subrepository
     # Fast forward is attempted and command aborted if this fails.
-    navigate_to_container_root()
+    navigate_to(configuration()["container_git_root"])
     # Push the changes to the subrepository and log the generated output
-    cmd = git["subrepo", "-dv", "pull", configuration()["feature_git_root"]]
+    cmd = git["subrepo", SUBREPO_VERBOSITY, "pull", configuration()["feature_git_root"]]
     run_command(cmd)
     # TODO: Error handling?
 
@@ -42,14 +45,24 @@ def pull_subrepo():
 def pull_container():
     # TODO: add the option to be much more specific with the new version you want, tags etc...
     logger().info("Pulling container...")
-    navigate_to_container_root()
+    navigate_to(configuration()["container_git_root"])
     cmd = git["pull"]
     run_command(cmd)
 
 
 def embed_subpreo():
-    pass
+    # This will clear/create the embedded feature directory
+    # and freshly clone the subrepository into this space.
+    # Clear and recreate subrepo root
+    feature_root = configuration()["feature_git_root"]
+    if os.path.isdir(feature_root):
+        run_command(local["rm"]["-r", feature_root])
+    run_command(local["mkdir"][feature_root])
+    navigate_to(feature_root)
+    # fresh clone
+    run_command(git["subrepo", SUBREPO_VERBOSITY, "clone", configuration()["feature_git_remote"]])
 
 
 def initialize_subrepo():
-    pass
+    navigate_to(configuration()["feature_git_root"])
+    run_command(git["subrepo", SUBREPO_VERBOSITY, "init"])
