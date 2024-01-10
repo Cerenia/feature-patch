3767df777c041e12383282227b7e2e894a7b2bb7
Christelle Gloor <christelle.gloor@inf.ethz.ch>
2024-01-10 07:04:35 +0100

    upgrade and cloning again also work

--- fp.py
+++ fp.py
@@ -7,13 +7,12 @@ from featurePatch.log import *
 
 def main():
     clean_subrepo()
-    create_migration_branch("v1.1.1")
-    checkout_subprepo("migration_v1.1.1")
-    extract_feature(True)
-    # Something went wrong here...
-    push_subrepo("Extracted contact points")
-    #upgrade_container_to("v1.1.1")
+    #create_migration_branch("v1.1.1")
     #checkout_subprepo("migration_v1.1.1")
+    #extract_feature(True)
+    #push_subrepo("Extracted contact points")
+    #upgrade_container_to("v1.1.1")
+    checkout_subprepo("migration_v1.1.1")
     # Attempt to apply
     # Clean up by hand
     # Merge subrepo back into master & checkout master
